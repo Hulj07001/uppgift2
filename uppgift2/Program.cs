f3dcba199c9ea61d353485a82909c4b265f8ac44
hulj07001 <hulj07001@E-PW05CGR7>
2024-09-12 12:29:13 +0200

    Add project files.

---
+++ uppgift2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ändrar färgen till blå
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            //ändrar textfärgen till vit
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(":(");
+            Console.WriteLine("Windows krashade på grund av oväntat fel");
+            Console.WriteLine("datorn måste startas om");
+        }
+    }
+}
